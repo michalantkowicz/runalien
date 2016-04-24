843f565b5471dd774a7551d28fe408506362187a
Mateusz Gaweł <mg.mateuszgawel@gmail.com>
2016-04-24 13:44:26 +0200

    First stage of working google services. (BaseGameUtils and other libs)

--- android/src/com/apptogo/runalien/AndroidLauncher.java
+++ android/src/com/apptogo/runalien/AndroidLauncher.java
@@ -1,12 +1,20 @@
 package com.apptogo.runalien;
 
-import android.os.Bundle;
-
+import com.apptogo.runalien.main.Main;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.apptogo.runalien.main.Main;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.games.Games;
 
-public class AndroidLauncher extends AndroidApplication {
+import android.os.Bundle;
+
+public class AndroidLauncher extends AndroidApplication implements OnConnectionFailedListener, ConnectionCallbacks{
+	
+	private GoogleApiClient mGoogleApiClient;
+	
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -14,5 +22,30 @@ public class AndroidLauncher extends AndroidApplication {
 		config.useImmersiveMode = true;
 		
 		initialize(new Main(), config);
+		
+	    // Create the Google Api Client with access to the Play Games services
+	    mGoogleApiClient = new GoogleApiClient.Builder(this)
+	            .addConnectionCallbacks(this)
+	            .addOnConnectionFailedListener(this)
+	            .addApi(Games.API).addScope(Games.SCOPE_GAMES)
+	            // add other APIs and scopes here as needed
+	            .build();
+		
+		mGoogleApiClient.connect();
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+		System.out.println("COS SIE ZJEBALO");
+	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		System.out.println("COS SIE POLACZYLO");
+	}
+
+	@Override
+	public void onConnectionSuspended(int arg0) {
+		System.out.println("COS SIE SUSPENDOWALO");		
 	}
 }
